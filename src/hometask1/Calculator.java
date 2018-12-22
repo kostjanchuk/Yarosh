7e4b5abbbe5823c2c14bb1fbf7ad6f5e2dfacc93
kostjanchuk <45865394+kostjanchuk@users.noreply.github.com>
2018-12-22 23:52:16 +0200

    Initial commit

---
+++ src/hometask1/Calculator.java
@@ -0,0 +1,34 @@
+package hometask1;
+
+public class Calculator {
+
+    public static int Add(int a , int b) {
+        return a + b;
+    }
+    public static double Add(double a , double b){
+        return a+b;
+    }
+    public static int Sub(int a , int b){
+        return a-b;
+    }
+
+    public static double Sub(double a , double b){
+        return a-b;
+    }
+    public static int Div(int a , int b){
+        return a/b;
+
+    }
+
+    public static double Div(double a , double b){
+        return a/b;
+    }
+    public static int Mul(int a , int b){
+        return a*b;
+    }
+
+
+    public static double Mul(double a , double b){
+        return a*b;
+    }
+}
---
+++ src/hometask1/Start.java
@@ -0,0 +1,12 @@
+package hometask1;
+
+public class Start {
+        public static void main(String[] args) {
+
+            System.out.println(Calculator.Add(5,3));
+            System.out.println(Calculator.Div(5D,3.3));
+            System.out.println(Calculator.Mul(5.2,3));
+            System.out.println(Calculator.Sub(5,3));
+
+        }
+}
