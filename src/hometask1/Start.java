b4723cc9043ae2b9b0fcbe1285e5fbaa9c04de59
kostjanchuk <45865394+kostjanchuk@users.noreply.github.com>
2018-12-23 00:03:38 +0200

    1

--- src/hometask1/Start.java
+++ src/hometask1/Start.java
@@ -3,10 +3,10 @@ package hometask1;
 public class Start {
         public static void main(String[] args) {
 
-            System.out.println(Calculator.Add(5,3));
-            System.out.println(Calculator.Div(5D,3.3));
-            System.out.println(Calculator.Mul(5.2,3));
-            System.out.println(Calculator.Sub(5,3));
+            System.out.println("5+3= " +Calculator.Add(5,3));
+            System.out.println("5/3.3= " +Calculator.Div(5D,3.3));
+            System.out.println("5*3= " +Calculator.Mul(5,3));
+            System.out.println("5-3= " +Calculator.Sub(5,3));
 
         }
 }
