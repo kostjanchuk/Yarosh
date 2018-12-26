10a50ff91cd2cc55e0d86ac22d7cc3374a22e172
kostjanchuk <45865394+kostjanchuk@users.noreply.github.com>
2018-12-26 15:36:27 +0200

    1

---
+++ src/hometask3/Start.java
@@ -0,0 +1,23 @@
+package hometask3;
+
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("введите число: ");
+        int b = in.nextInt();
+
+
+     if  ( b%2==1)
+    {
+        System.out.println(b+" -нечётное число");
+    }
+    else
+    {
+        System.out.println(b+" -чётное число");
+    }
+}
+    }
