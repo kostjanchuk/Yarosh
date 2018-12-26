1410cc9459a16f42b0a6ed72f6478eeed99a2437
kostjanchuk <45865394+kostjanchuk@users.noreply.github.com>
2018-12-26 15:33:57 +0200

    1

---
+++ src/hometask2/Start.java
@@ -0,0 +1,20 @@
+package hometask2;
+
+public class Start {
+
+
+        public static void main(String[] args) {
+
+            System.out.println("a + b * ( c / 2 )=  " +Calc.methodCalc1(5,3,4));
+
+
+            System.out.println("a + b * ( c / 2 )=  " +Calc.methodCalc2(5,3));
+
+
+            System.out.println("a + b * ( c / 2 )=  " +Calc.methodCalc3(5,3,4));
+
+            System.out.println("a + b * ( c / 2 )=  " +Calc.methodCalc4(5,3,4));
+
+
+        }
+}
