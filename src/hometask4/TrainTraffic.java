e289930dc141fbdb55b618558ca3ecfe5b8697fb
kostjanchuk <45865394+kostjanchuk@users.noreply.github.com>
2019-01-18 23:08:20 +0200

    1

--- src/hometask4/TrainTraffic.java
+++ src/hometask4/TrainTraffic.java
@@ -6,9 +6,9 @@ public class TrainTraffic {
     public static boolean trainColisiom() {
         int S = 10;
         System.out.println("Скорость 1 поезда");
-        int V1 = InputKey.inputKey();
+        double V1 = InputKey.inputKey();
         System.out.println("Скорость 2 поезда");
-        int V2 = InputKey.inputKey();
+        double V2 = InputKey.inputKey();
         double T=S/(V1+V2);
         if (T*V1<= 4 && T*V2>=6) {
             System.out.println("столкнутся");
--- src/hometask4/start.java
+++ src/hometask4/start.java
@@ -3,18 +3,19 @@ package hometask4;
 import Utilities.InputKey;
 
 
-public class start {
+public class
+        start {
 
 
     public static void main(String[] args) {
-        System.out.println(Converter.MetodConv()+ " грн");
-        System.out.println("Cумма цифр числа  =  " + Summation.sum());
-        System.out.println("Введите ширину участка");
-        int a = InputKey.inputKey();
-        System.out.println("Введите длину участка");
-        int b = InputKey.inputKey();
-        PlotOfLand.Perimetr(a,b);
-        PlotOfLand.Square(a,b);
+      //  System.out.println(Converter.MetodConv()+ " грн");
+       // System.out.println("Cумма цифр числа  =  " + Summation.sum());
+       // System.out.println("Введите ширину участка");
+       // int a = InputKey.inputKey();
+       // System.out.println("Введите длину участка");
+       // int b = InputKey.inputKey();
+      //  PlotOfLand.Perimetr(a,b);
+       //1 PlotOfLand.Square(a,b);
         TrainTraffic.trainColisiom();
 
 
