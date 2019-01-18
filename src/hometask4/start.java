1af0c46dd76ec566d7081c23328dc6d84e315b95
kostjanchuk <45865394+kostjanchuk@users.noreply.github.com>
2019-01-18 23:11:37 +0200

    1

--- src/hometask4/start.java
+++ src/hometask4/start.java
@@ -8,14 +8,14 @@ public class
 
 
     public static void main(String[] args) {
-      //  System.out.println(Converter.MetodConv()+ " грн");
-       // System.out.println("Cумма цифр числа  =  " + Summation.sum());
-       // System.out.println("Введите ширину участка");
-       // int a = InputKey.inputKey();
-       // System.out.println("Введите длину участка");
-       // int b = InputKey.inputKey();
-      //  PlotOfLand.Perimetr(a,b);
-       //1 PlotOfLand.Square(a,b);
+        System.out.println(Converter.MetodConv()+ " грн");
+        System.out.println("Cумма цифр числа  =  " + Summation.sum());
+        System.out.println("Введите ширину участка");
+        int a = InputKey.inputKey();
+        System.out.println("Введите длину участка");
+        int b = InputKey.inputKey();
+      PlotOfLand.Perimetr(a,b);
+        PlotOfLand.Square(a,b);
         TrainTraffic.trainColisiom();
 
 
