8dd7dac1c230002082febfe805ed2a4fc754d051
kostjanchuk <45865394+kostjanchuk@users.noreply.github.com>
2019-01-18 23:43:13 +0200

    1

--- src/hometask10/Start.java
+++ src/hometask10/Start.java
@@ -7,7 +7,7 @@ public class Start {
         int [] a= {1,3,4,5,6,2};
         int [] f= {1,3,4,5,6,4};
         int [] z= {1,3,4,5,6};
-        double [] c= {1,2,3,4,5,6,7};
+        double [] c= {1,2,3,4,5,6,1};
         Tasks.difference(a);
         char[]  charr =  {'X','d','I','j','Y'};
 Tasks.swapElements(charr);
